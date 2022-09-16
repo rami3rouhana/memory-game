6884046c69e01edacedf9e3d1bd0e287dc8e2a0f
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-16 12:29:11 +0300

    add card selection

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// Images name
 const images = ["c", "c++", "java", "javascript", "python"];
+
+
+// Start game
+const startGame = ()=>{
+
+    // Cards Selection
+    const firstPair = Math.random() * 5;
+    do {
+        const secondPair = Math.random() * 5;
+    } while (firstPair!=secondPair);
+    do {
+        const thirdPair = Math.random() * 5;
+    } while (firstPair!=thirdPair && thirdPair!=secondPair);
+
+
+    const cards = document.getElementsByClassName("card");
+
+    cards.forEach(element => {
+        
+    });
+}
+
+document.getElementById("start").addEventListener("click", startGame)
